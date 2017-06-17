80ea0b70090cd7032d5c34b8e74ee7b4e0327b7a
Ignacio <jinavaa@hotmail.com>
2017-06-17 17:05:26 +0000

    LED code added

---
+++ LED.c
@@ -0,0 +1,29 @@
+/* Cuando el GPIO02 esté en alto entonces apagar el led conectado al GPIO05 */
+
+#include <stdio.h>
+#include <pigpio.h>
+
+#define IN  02 /* GPIO02 como entrada */
+#define OUT 05 /* GPIO05 como salida */
+
+int read_1;
+
+int main (int argc, char *argv[])
+{
+        if(gpioInitialise()<0) return -1; /*Inicializando pigpio*/
+
+gpioSetMode(IN, PI_INPUT);
+gpioSetMode(OUT, PI_OUTPUT);
+
+for(;;)
+{
+        read_1 = gpioRead(IN);
+        gpioWrite(OUT,1);
+
+        if(read_1 == 0)
+        {
+            gpioWrite(OUT,0);
+        }
+}
+gpioTerminate();
+
